20dcde3d8e125e7336b043e72c8774f8765d43da
Matheus <matheusxp@icloud.com>
2019-05-28 02:27:53 -0300

    first commit

---
+++ src/Cache/main.java
@@ -0,0 +1,9 @@
+package Cache;
+
+public class main {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/Cache/palavra.java
@@ -0,0 +1,37 @@
+package Cache;
+
+public class palavra {
+    
+    private int bitVal, tag, dados;
+
+    public palavra(int bitVal, int tag, int dados) {
+        this.bitVal = bitVal;
+        this.tag = tag;
+        this.dados = dados;
+    }
+    
+    public int getBitVal() {
+        return bitVal;
+    }
+
+    public void setBitVal(int bitVal) {
+        this.bitVal = bitVal;
+    }
+
+    public int getTag() {
+        return tag;
+    }
+
+    public void setTag(int tag) {
+        this.tag = tag;
+    }
+
+    public int getDados() {
+        return dados;
+    }
+
+    public void setDados(int dados) {
+        this.dados = dados;
+    }
+    
+}
